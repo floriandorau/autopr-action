a5897950998c1c5792b91afb3d737894b9abf407
Florian Dorau <fdorau@it-economics.de>
2019-02-14 14:48:44 +0100

    Added tests

--- test/env.test.js
+++ test/env.test.js
@@ -1,27 +1,43 @@
 const env = require('../lib/env');
+const { Failure } = require('../lib/errors');
 
 beforeEach(() => delete process.env.TEST_ENV_VAR);
 
-test('extractVar returns value for given env var', () => {
-    process.env.TEST_ENV_VAR='test';
+describe('extractVar', () => {
+    test('returns value for given env var', () => {
+        process.env.TEST_ENV_VAR = 'test';
 
-    const result = env.extractVar('TEST_ENV_VAR');
-    expect(result).toBe('test');
+        const result = env.extractVar('TEST_ENV_VAR');
+        expect(result).toBe('test');
+    });
+
+    test('does not return default value for given env var', () => {
+        process.env.TEST_ENV_VAR = 'test';
+
+        const result = env.extractVar('TEST_ENV_VAR', 'defaultTest');
+        expect(result).toBe('test');
+    });
+
+    test('returns undefined for not given env var', () => {
+        const result = env.extractVar('TEST_ENV_VAR');
+        expect(result).toBeUndefined();
+    });
+
+    test('returns default value for not given env var', () => {
+        const result = env.extractVar('TEST_ENV_VAR', 'defaultTest');
+        expect(result).toBe('defaultTest');
+    });
 });
 
-test('extractVar does not return default value for given env var', () => {
-    process.env.TEST_ENV_VAR='test';
+describe('extractRequiredVar', () => {
+    test('returns value for given env var', () => {
+        process.env.TEST_ENV_VAR = 'test';
 
-    const result = env.extractVar('TEST_ENV_VAR', 'defaultTest');
-    expect(result).toBe('test');
-});
+        const result = env.extractRequiredVar('TEST_ENV_VAR');
+        expect(result).toBe('test');
+    });
 
-test('extractVar returns undefined for not given env var', () => {
-    const result = env.extractVar('TEST_ENV_VAR');
-    expect(result).toBeUndefined();
-});
-
-test('extractVar returns default value for not given env var', () => {
-    const result = env.extractVar('TEST_ENV_VAR', 'defaultTest');
-    expect(result).toBe('defaultTest');
+    test('throws error for not defined env var', () => {
+        expect(() => env.extractRequiredVar('TEST_ENV_VAR')).toThrow("required env variable 'TEST_ENV_VAR' not defined");
+    });
 });
